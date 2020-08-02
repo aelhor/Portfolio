ed609d9059f3feefbbdead54635b62ba502bacf5
Ahmed Elhor <aelhor90@gmail.com>
2020-08-02 14:29:07 +0200

    Updates

---
+++ precache-manifest.359d21622f4a01aca6693503f8bb8eae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e677e86387ae0efa67e0f8d69dcf05",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c5fd0a9f34633f36def4",
+    "url": "/Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "22371caabaffcb23963c",
+    "url": "/Portfolio/static/css/main.059fccef.chunk.css"
+  },
+  {
+    "revision": "c5fd0a9f34633f36def4",
+    "url": "/Portfolio/static/js/2.06d952c6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.06d952c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22371caabaffcb23963c",
+    "url": "/Portfolio/static/js/main.4d2ebec5.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
